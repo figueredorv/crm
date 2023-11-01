eb497f6aa34599a67af17f2ce7ea3ca89513f70d
Ruan Victor Figueredo <vitinhomusic2014@gmail.com>
2023-11-01 00:18:01 -0300

    Alterações

--- profile.php
+++ profile.php
@@ -23,19 +23,19 @@ include("conexao.php");
 
 
     <title>
-    CRM CORBAN
-  </title>
-  <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
-  <!--     Fonts and icons     -->
-  <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
-  <link href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
-  <!-- CSS Files -->
-  <link href="assets/css/bootstrap.min.css" rel="stylesheet" />
-  <link href="assets/css/paper-dashboard.css?v=2.0.0" rel="stylesheet" />
-  <!-- CSS Just for demo purpose, don't include it in your project -->
-  <link href="assets/demo/demo.css" rel="stylesheet" />
-  <link rel="stylesheet" href="css/painel-funcionario.css">
-  <link rel="stylesheet" href="css/style.css">
+        CRM CORBAN
+    </title>
+    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
+    <!--     Fonts and icons     -->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
+    <!-- CSS Files -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="assets/css/paper-dashboard.css?v=2.0.0" rel="stylesheet" />
+    <!-- CSS Just for demo purpose, don't include it in your project -->
+    <link href="assets/demo/demo.css" rel="stylesheet" />
+    <link rel="stylesheet" href="css/painel-funcionario.css">
+    <link rel="stylesheet" href="css/style.css">
 
 </head>
 
@@ -395,7 +395,12 @@ include("conexao.php");
                                                                 $nome = $res_1["usuario"];
                                                                 $propostas = $res_1["propostas"];
                                                                 $idUsuario = $res_1["idusuarios"];
+
+
+
+
                                                                 $caminhoDaImagem = 'https://mdbootstrap.com/img/Photos/Others/placeholder-avatar.jpg'; // Caminho padrão para imagem de placeholder
+
 
                                                                 // Verifique se o usuário tem uma imagem de perfil
                                                                 $sql = "SELECT imagem FROM usuarios WHERE idusuarios = $idUsuario";
@@ -409,10 +414,13 @@ include("conexao.php");
                                                             ?>
                                                                 <tr>
                                                                     <td>
-                                                                        <img class="avatar border-gray" src="<?php echo $caminhoDaImagem; ?>" alt="Imagem de Perfil">
+                                                                        <img class="avatar border-gray" src="<?php echo $caminhoDaImagem; ?>" alt="Imagem de Perfil" style="vertical-align: middle;">
+                                                                        <span style="vertical-align: middle; margin-right: 5px;"></span>
                                                                     </td>
+                                                                    <td><?php echo $nome ?></td>
 
-                                                                    <td><?php echo $nome; ?></td>
+
+
 
                                                                 </tr>
                                                             <?php
