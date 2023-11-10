e95a46882eccd7494663075526c8abf7f279491c
Ruan Victor Figueredo <vitinhomusic2014@gmail.com>
2023-11-10 18:30:25 -0300

    Update painel_funcionario.php

--- painel_funcionario.php
+++ painel_funcionario.php
@@ -1040,3 +1040,25 @@ if (@$_GET['func'] == 'deletaproposta') {
   }
 }
 ?>
+
+
+<!-- Modal de confirmação de exclusão de registro -->
+<div class="modal fade" id="confirmModal" tabindex="-1" role="dialog" aria-labelledby="confirmModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="confirmModalLabel">Confirmação de Exclusão</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <p>Tem certeza de que deseja excluir este registro?</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
+        <a class="btn btn-primary btn btn-danger" href="propostas.php?func=deleta&id=<?php echo $id; ?>">Excluir</a>
+      </div>
+    </div>
+  </div>
+</div>
