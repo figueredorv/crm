c1aa23b8652e46608d6aababb25e4fab0e1fa5f4
Ruan Victor Figueredo <vitinhomusic2014@gmail.com>
2023-11-10 23:41:48 -0300

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,6 +1,14 @@
-<?php 
 
+<?php 
+include('conexao.php');
 session_start();
+
+// Atualize a coluna 'ultima_autenticacao' com a data e hora atuais
+$idUsuario =  $_SESSION['idusuarios'];
+$query = "UPDATE usuarios SET ultima_autenticacao = '0000-00-00' WHERE idusuarios = $idUsuario";
+mysqli_query($conexao, $query);
+
+
 session_destroy();
 header('Location:index.php');
 	exit();
