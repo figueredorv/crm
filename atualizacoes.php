a75b6b9b58b46a381cf0bde80a522c0f6e02620b
Ruan Victor Figueredo <vitinhomusic2014@gmail.com>
2023-11-16 22:59:15 -0300

    Create atualizacoes.php

---
+++ atualizacoes.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>Atualizações CRMCORBAN</title>
+  <!-- Adicione os links para os arquivos do Bootstrap -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.2/dist/umd/popper.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <!-- Adicione o seu próprio estilo personalizado aqui -->
+  <style>
+    body {
+      padding: 20px;
+    }
+    .update-card {
+      margin-bottom: 20px;
+    }
+  </style>
+</head>
+<body>
+
+  <div class="container">
+    <h1 class="mt-4 mb-4">Atualizações CRMCORBAN</h1>
+
+    <!-- Exemplo de uma atualização -->
+    <div class="card update-card">
+      <div class="card-body">
+        <h5 class="card-title">Versão 1.1.0 - Nome da Atualização</h5>
+        <p class="card-text">Descrição da atualização e novos recursos implementados no CRMCORBAN.</p>
+      </div>
+    </div>
+
+    <!-- Adicione mais atualizações conforme necessário -->
+    
+  </div>
+
+</body>
+</html>
