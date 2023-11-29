14f2d0e6a5ac425f39665142b13e0ead1bff9e0d
Ruan Victor Figueredo <vitinhomusic2014@gmail.com>
2023-11-29 00:28:39 -0300

    Update painel_funcionario.php

--- painel_funcionario.php
+++ painel_funcionario.php
@@ -40,7 +40,7 @@ include("conexao.php");
         <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
             <!-- Sidebar - Brand -->
-            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="#">
                 <div class="sidebar-brand-icon rotate-n-15">
                     <i class="fas fa-laugh-wink"></i>
                 </div>
@@ -52,7 +52,7 @@ include("conexao.php");
 
             <!-- Nav Item - Dashboard -->
             <li class="nav-item active">
-                <a class="nav-link" href="index.html">
+                <a class="nav-link" href="painel_funcionario.php">
                     <i class="fas fa-fw fa-tachometer-alt"></i>
                     <span>Dashboard</span></a>
             </li>
