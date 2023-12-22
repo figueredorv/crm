2e43a69dd4a60789f8c84f2521bdbc430df1ef70
Ruan Victor Figueredo <vitinhomusic2014@gmail.com>
2023-12-22 18:43:46 -0300

    Update propostas.php

--- propostas.php
+++ propostas.php
@@ -45,9 +45,7 @@ $nomeusuario = $_SESSION['nome_usuario'];
   <!-- Core plugin JavaScript-->
   <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
 
-  <!-- Custom scripts for all pages-->
-  <script src="js/sb-admin-2.min.js"></script>
-
+ 
   <!--     Fonts and icons     -->
   <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
   <link href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
@@ -1623,6 +1621,8 @@ $nomeusuario = $_SESSION['nome_usuario'];
     </div>
   </div>
 
+ <!-- Custom scripts for all pages-->
+ <script src="js/sb-admin-2.min.js"></script>
 
 
 </body>
