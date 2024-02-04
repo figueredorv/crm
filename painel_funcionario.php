4428befbb7f9ed7aa87f78d4d6d4d477652ae694
Ruan Victor Figueredo <vitinhomusic2014@gmail.com>
2024-02-04 13:57:22 -0300

    Update painel_funcionario.php

--- painel_funcionario.php
+++ painel_funcionario.php
@@ -337,7 +337,10 @@ include("conexao.php");
                     <!-- Page Heading -->
                     <div class="d-sm-flex align-items-center justify-content-between mb-4">
                         <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
-                        <button class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm" disabled><i class="fas fa-download fa-sm text-white-50"></i> Gerar relatório</button>
+                        <button onclick="window.open('relatorio.php', '_blank')" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm">
+    <i class="fas fa-download fa-sm text-white"></i> Gerar relatório
+</button>
+
                     </div>
 
                     <?php
